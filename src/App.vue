d01b75aeae052ee0a347a585a03c312ad53eb17b
vahid-nejad <vahid884@gmail.com>
2022-10-25 11:37:44 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Sidebar from "./Sidebar.vue";
+const show = ref(false);
+</script>
+
+<template>
+  <button
+    class="bg-violet-500 py-2 px-4 text-white rounded-md hover:bg-violet-600"
+    v-on:click="show = !show"
+  >
+    Open Sidebar
+  </button>
+  <Sidebar :show="show" v-on:close="show = false">
+    <h3
+      class="text-center bg-gradient-to-b from-sky-400 to-sky-600 text-white py-2"
+    >
+      This is a sidebar
+    </h3>
+  </Sidebar>
+</template>
---
+++ src/Sidebar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{ show: boolean }>();
+defineEmits(["close"]);
+</script>
+<template>
+  <div
+    v-if="show"
+    class="h-screen fixed top-0 right-0 w-full bg-black/25 backdrop-blur"
+  ></div>
+  <div
+    class="fixed bg-slate-200 w-48 h-screen top-0 transition-all duration-500"
+    :class="show ? 'right-0' : '-right-full'"
+  >
+    <div
+      v-on:click="$emit('close')"
+      class="absolute cursor-pointer m-1 hover:text-red-500 transition"
+    >
+      x
+    </div>
+    <slot></slot>
+  </div>
+</template>
